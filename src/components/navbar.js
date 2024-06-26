c42ba3cbb1c4c7757c9d891bc3afec9a13eb28aa
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2024-06-26 22:49:25 +0900

    fixed bugs in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,15 @@
-import React,{Component} from 'react';
-import jelani from '../assets/img/jelaniprofilepic.jpeg'
-import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
-import {Navbar,Nav} from 'react-bootstrap'
-import {Container,Row,Col} from 'react-bootstrap';
-import BootstrapSwitchButton from 'bootstrap-switch-button-react'
+import React, { Component } from 'react';
+import jelani from '../assets/img/jelaniprofilepic.jpeg';
+import { Link, animateScroll as scroll } from 'react-scroll';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import BootstrapSwitchButton from 'bootstrap-switch-button-react';
 
 class Navigate extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            list_of_titles : [
+            list_of_titles: [
                 "about",
                 "experience",
                 "education",
@@ -17,48 +17,54 @@ class Navigate extends Component {
                 "interests",
                 "blog"
             ],
-            
         };
     }
-    
-    render() {
-        const titles = this.props.titles.map((title,index) =>
-        <Container style={{paddingRight: 0, marginRight: 0}} className="nav-item justify-content-end">
-        <Nav.Link as={Link} eventKey={index} href="#" activeClass="active" style={{cursor:"pointer"}} className="nav-link" spy={true} to={this.props.hrefs[index]} smooth={"easeInOutQuart"} duration={1500}>{title}</Nav.Link>
-        </Container>
-        );
-        return(
 
-          <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav" collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <BootstrapSwitchButton
-        checked={false}
-        onlabel='Eng'
-        offlabel='JP'
-        checked={true}
-        size="lg"
-        onstyle="dark" offstyle="light"
-        onChange={() => {
-          this.props.handleSwitch()
-        }}
-        />
-              <button className="navbar-brand" onClick={() => scroll.scrollToTop()} style={{ background: 'none', border: 'none', padding: 0 }}>
+    render() {
+        const titles = this.props.titles.map((title, index) => (
+            <Container style={{ paddingRight: 0, marginRight: 0 }} className="nav-item justify-content-end" key={index}>
+                <Nav.Link
+                    as={Link}
+                    eventKey={index}
+                    activeClass="active"
+                    style={{ cursor: "pointer" }}
+                    className="nav-link"
+                    spy={true}
+                    to={this.props.hrefs[index]}
+                    smooth={"easeInOutQuart"}
+                    duration={1500}
+                >
+                    {title}
+                </Nav.Link>
+            </Container>
+        ));
+        return (
+            <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav" collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <BootstrapSwitchButton
+                    checked={false}
+                    onlabel='Eng'
+                    offlabel='JP'
+                    size="lg"
+                    onstyle="dark"
+                    offstyle="light"
+                    onChange={() => {
+                        this.props.handleSwitch();
+                    }}
+                />
+                <button className="navbar-brand" onClick={() => scroll.scrollToTop()} style={{ background: 'none', border: 'none', padding: 0 }}>
                     <span className="d-none d-lg-block">
-                        <img className="img-fluid img-profile rounded-circle mx-auto mb-2" style={{cursor: "pointer"}}
-                             src={jelani} alt=""/>
+                        <img className="img-fluid img-profile rounded-circle mx-auto mb-2" style={{ cursor: "pointer" }} src={jelani} alt="" />
                     </span>
-              </button>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-              <Navbar.Collapse id="responsive-navbar-nav">
-                  <Nav className="mr-auto">
-                      {titles}
-                  </Nav>
-          </Navbar.Collapse>
-          </Navbar>
-            
-          
-          );
+                </button>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        {titles}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
     }
 }
 
-
-export default Navigate
+export default Navigate;
