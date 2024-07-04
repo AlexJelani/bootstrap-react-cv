ae23354cd72df311a50458a3576df3f7c1d2c511
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2024-07-04 20:20:36 +0900

    added Lambda http api to RetroHitCounter function

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
+import RetroHitCounter from 'react-retro-hit-counter';
 import jelani from '../assets/img/jelaniprofilepic.jpeg';
 import { Link, animateScroll as scroll } from 'react-scroll';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
-// import BootstrapSwitchButton from 'bootstrap-switch-button-react';
 
 class Navigate extends Component {
     constructor(props) {
@@ -17,7 +17,16 @@ class Navigate extends Component {
                 "interests",
                 "blog"
             ],
+            hits: 0  // Initialize hits in state
         };
+    }
+
+    componentDidMount() {
+        // Fetch hits count when component mounts
+        fetch('https://jo9xko8ca7.execute-api.us-east-1.amazonaws.com/default/hit_counter')
+            .then(response => response.json())
+            .then(data => this.setState({ hits: data.hits }))
+            .catch(error => console.error('Error fetching hits:', error));
     }
 
     render() {
@@ -38,19 +47,9 @@ class Navigate extends Component {
                 </Nav.Link>
             </Container>
         ));
+
         return (
             <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav" collapseOnSelect expand="lg" bg="dark" variant="dark">
-                {/*<BootstrapSwitchButton*/}
-                {/*    checked={false}*/}
-                {/*    onlabel='Eng'*/}
-                {/*    offlabel='JP'*/}
-                {/*    size="lg"*/}
-                {/*    onstyle="dark"*/}
-                {/*    offstyle="light"*/}
-                {/*    onChange={() => {*/}
-                {/*        this.props.handleSwitch();*/}
-                {/*    }}*/}
-                {/*/>*/}
                 <button className="navbar-brand" onClick={() => scroll.scrollToTop()} style={{ background: 'none', border: 'none', padding: 0 }}>
                     <span className="d-none d-lg-block">
                         <img className="img-fluid img-profile rounded-circle mx-auto mb-2" style={{ cursor: "pointer" }} src={jelani} alt="" />
@@ -60,11 +59,30 @@ class Navigate extends Component {
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
                         {titles}
+                        {/* Put retro counter here */}
+                        <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
+                            <RetroHitCounter
+                                hits={this.state.hits} // Use hits from state
+                                withBorder={false}
+                                withGlow={false}
+                                minLength={4}
+                                size={40}
+                                padding={4}
+                                digitSpacing={3}
+                                segmentThickness={4}
+                                segmentSpacing={0.5}
+                                segmentActiveColor="#76FF03"
+                                segmentInactiveColor="#315324"
+                                backgroundColor="#222222"
+                                borderThickness={7}
+                                glowStrength={0.5}
+                            />
+                        </div>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-        );
+    );
     }
-}
+    }
 
-export default Navigate;
+    export default Navigate;
